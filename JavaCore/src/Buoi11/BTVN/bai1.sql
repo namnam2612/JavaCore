5db3c40a4fed87977aa9667a60eae02b579e2108
Java <danghoangnam2612@gmail.com>
2024-12-25 21:41:27 +0700

    comit btvn b11

--- JavaCore/src/Buoi11/BTVN/bai1.sql
+++ JavaCore/src/Buoi11/BTVN/bai1.sql
@@ -29,7 +29,7 @@ create table Enrollments (
 insert into student (id, first_name, last_name, birth_date, email)
     value (1,'Nam', 'Hoang','2003-12-26','danghoangnam@gmail.com');
 insert into student (id, first_name, last_name, birth_date, email)
-    value (2,'Thao', 'Huyen','2004-07-15','thaohuyen@gmail.com');
+    value (2,'Thao', 'Huyen','2005-07-15','thaohuyen@gmail.com');
 insert into student (id, first_name, last_name, birth_date, email)
     value (3,'Phong', 'Le','2004-04-11','phongle@gmail.com');
 insert into student (id, first_name, last_name, birth_date, email)
@@ -55,20 +55,20 @@ insert into Enrollments (student_id, course_id, enrollment_date)
 insert into Enrollments (student_id, course_id, enrollment_date)
     value (4, 4, '2024-04-12');
 
--- 1
+-- 1 truy van thong tin sinh vien (ho,ten, email)
 select student.first_name, student.last_name, student.email from student;
--- 2
+-- 2 truy van tat ca khoa hoc (ten khoa hoc, mo ta)
 select * from student;
 select * from  course;
 select * from enrollments;
--- 3
+-- 3 Truy vấn danh sách sinh viên cùng khóa học họ đăng ký (bao gồm tên sinh viên và tên khóa học).
 select student.first_name as student_first_name,
        student.last_name as student_last_name,
        course.course_name as course_name
 from enrollments
          join student on enrollments.student_id = student.id
          join course on enrollments.course_id = course.id;
--- 4
+-- 4 Truy vấn danh sách sinh viên đã đăng ký khóa học "Lập Trình Cơ Bản".
 select student.first_name as student_first_name,
        student.last_name as student_last_name,
        student.email
@@ -76,25 +76,48 @@ from enrollments
          join student on enrollments.student_id = student.id
          join course on enrollments.course_id = course.id
 where course.course_name like 'lap trinh co ban';
--- 5
+-- 5 Truy vấn các sinh viên có ngày sinh sau ngày 2000-01-01 và có email chứa từ "gmail"
 select student.first_name, student.last_name, student.email
 from student
 where birth_date > '2000-01-01' and email like 'gmail';
 
+-- Cập nhật email của sinh viên có student_id = 1 thành một địa chỉ email mới.
 update student
 set email = 'nguyenminhkhanhnam@gmail.com'
 where id = 1;
 
+-- Cập nhật mô tả của khóa học "Lập Trình Cơ Bản" thành mô tả mới.
 update course
 set course_description = 'mo ta moi cho khoa hoc lap trinh co ban'
-where course_name - 'lap trinh co ban';
+where course_name = 'lap trinh co ban';
 
+-- Xóa một sinh viên cụ thể khỏi bảng Students (chỉ xóa nếu sinh viên không có bất kỳ khóa học nào đã đăng ký).
 DELETE FROM student
 WHERE id = 1 AND id NOT IN (SELECT student_id FROM enrollments);
 
+-- Xóa một khóa học khỏi bảng Courses (chỉ xóa nếu khóa học đó không có sinh viên nào đã đăng ký).
 DELETE FROM course
 WHERE id = 1 AND id NOT IN (SELECT course_id FROM enrollments);
 
+-- truy van cac sinh vien co ngay sinh lon hon 2000-01-01
+select * from student where birth_date > '2000-01-01';
 
+-- truy van tat ca sinh vien co ten bat dau bang "Nguyen"
+select * from student where first_name like 'Nguyen%';
 
+-- truy van tat ca sinh vien co ngay sinh sau 2000-01-01 va da dang ky khoa hoc "toan hoc"
+select student.* from student
+join enrollments on student.id = Enrollments.student_id
+join course on course.id = enrollments.course_id
+where student.birth_date > '2000-01-01'
+and course.course_name = 'toan hoc';
+
+-- truy van cac sinh vien da dang ky khoa hoc co course_id trong danh sach 101,102
+select distinct student.* from student
+join enrollments on student.id = Enrollments.student_id
+where enrollments.course_id in (101,102);
+
+-- truy van cac khoa hoc co course_id ban trong khoang tu 101 den 103
+select * from course
+where id between 101 and 103;
 
